7e4d80610edd7501a59983b67d8ece1c0465648d
book0256@naver.com <book0256@naver.com>
2023-11-18 12:01:12 +0900

    여행 기록 기능 새 버전으로 업데이트 여행 기록 최종 저장 기능 수정 필요 여행 비용 기능 병합 필요

--- src/main/resources/static/js/app/updateRecord.js
+++ src/main/resources/static/js/app/updateRecord.js
@@ -1,96 +1,96 @@
 
-var recordId; // record content에서도 쓸거라 전역변수로 해줘야됨
+    var recordId; // record content에서도 쓸거라 전역변수로 해줘야됨
 
-$(document).ready(function () {
+    $(document).ready(function () {
 
-    var urlParams = new URLSearchParams(window.location.search);
-    recordId = urlParams.get('recordId');
+        var urlParams = new URLSearchParams(window.location.search);
+        recordId = urlParams.get('recordId');
 
-    $('#detailButton').attr('href', 'view_detail_record?recordId=' + recordId);
-    //이 여행 기록 상세보기로 가는 버튼
+         $('#detailButton').attr('href', 'view_detail_record?recordId=' + recordId);
+         //이 여행 기록 상세보기로 가는 버튼
 
-    $.get("/fetch-record/" + recordId, function (data) {
-        $('#recordTitleInput').val(data.recordTitle);
-        $('#locationInput').val(data.location);
+        $.get("/fetch-record/" + recordId, function (data) {
+            $('#recordTitleInput').val(data.recordTitle);
+            $('#locationInput').val(data.location);
 
-        var startDate = new Date(data.startDate).toISOString().split('T')[0];
-        var endDate = new Date(data.endDate).toISOString().split('T')[0];
-        $('#startDateInput').val(startDate);
-        $('#endDateInput').val(endDate);
-        $('#costInput').val(data.cost);
-    });
-
-
-
-    $('#saveBasicInfoButton').click(function() {
-        var updatedBasicInfo = {
-            recordTitle: $('#recordTitleInput').val(),
-            location: $('#locationInput').val(),
-            startDate: $('#startDateInput').val(),
-            endDate: $('#endDateInput').val(),
-            cost: $('#costInput').val()
-        };
-
-        $.ajax({
-            url: "/update-record/" + recordId,
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify(updatedBasicInfo),
-            success: function(response) {
-                alert('기본 정보가 성공적으로 업데이트되었습니다.');
-                location.reload();
-            },
-            error: function(response) {
-                alert('기본 정보 업데이트 중 오류가 발생했습니다.');
-            }
-        });
-    });
-
-    $('#updateButton').click(function() {
-        var updatedRecord = {
-            recordTitle: $('#recordTitleInput').val(),
-            location: $('#locationInput').val(),
-            startDate: $('#startDateInput').val(),
-            endDate: $('#endDateInput').val(),
-            cost: $('#costInput').val(),
-            content: []
-        };
-
-        $('#content').children().each(function(index, element) {
-            var date = $(element).find('input[type=date]').val();
-            var content = $(element).find('input[type=text]').val();
-            updatedRecord.content.push({date: date, content: content});
+            var startDate = new Date(data.startDate).toISOString().split('T')[0];
+            var endDate = new Date(data.endDate).toISOString().split('T')[0];
+            $('#startDateInput').val(startDate);
+            $('#endDateInput').val(endDate);
+            $('#costInput').val(data.cost);
         });
 
-        $.ajax({
-            url: "/update-record/" + recordId,
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify(updatedRecord),
-            success: function(response) {
-                alert('Record updated successfully.');
-            },
-            error: function(response) {
-                alert('Error updating record.');
-            }
+
+
+         $('#saveBasicInfoButton').click(function() {
+                var updatedBasicInfo = {
+                    recordTitle: $('#recordTitleInput').val(),
+                    location: $('#locationInput').val(),
+                    startDate: $('#startDateInput').val(),
+                    endDate: $('#endDateInput').val(),
+                    cost: $('#costInput').val()
+                };
+
+                $.ajax({
+                    url: "/update-record/" + recordId,
+                    type: 'PUT',
+                    contentType: 'application/json',
+                    data: JSON.stringify(updatedBasicInfo),
+                    success: function(response) {
+                        alert('기본 정보가 성공적으로 업데이트되었습니다.');
+                        location.reload();
+                    },
+                    error: function(response) {
+                        alert('기본 정보 업데이트 중 오류가 발생했습니다.');
+                    }
+                });
+            });
+
+        $('#updateButton').click(function() {
+            var updatedRecord = {
+                recordTitle: $('#recordTitleInput').val(),
+                location: $('#locationInput').val(),
+                startDate: $('#startDateInput').val(),
+                endDate: $('#endDateInput').val(),
+                cost: $('#costInput').val(),
+                content: []
+            };
+
+            $('#content').children().each(function(index, element) {
+                var date = $(element).find('input[type=date]').val();
+                var content = $(element).find('input[type=text]').val();
+                updatedRecord.content.push({date: date, content: content});
+            });
+
+            $.ajax({
+                url: "/update-record/" + recordId,
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(updatedRecord),
+                success: function(response) {
+                    alert('Record updated successfully.');
+                },
+                error: function(response) {
+                    alert('Error updating record.');
+                }
+            });
         });
+
+             $('#saveContentButton').click(function() {
+                     var updatedData = [];
+
+                     $('#content').children().each(function(index, element) {
+                         var date = $(element).find('input[type=date]').val();
+                         var content = $(element).find('input[type=text]').val();
+                         updatedData.push({date: date, content: content});
+                     });
+
+                     sendDataToServer(recordId, updatedData);
+                });
+
+         createInitialTemplate();
+
     });
-
-    $('#saveContentButton').click(function() {
-        var updatedData = [];
-
-        $('#content').children().each(function(index, element) {
-            var date = $(element).find('input[type=date]').val();
-            var content = $(element).find('input[type=text]').val();
-            updatedData.push({date: date, content: content});
-        });
-
-        sendDataToServer(recordId, updatedData);
-    });
-
-    createInitialTemplate();
-
-});
 $(document).ready(function () {
     // ... 기존 코드 ...
 
@@ -126,11 +126,11 @@ function sendDataToServer(recordId, data) { // date_content랑 약간 다름
 
     data.forEach(content => {
         //빈칸인 경우에는 DB에 그 날짜의 행을 업로드하고 싶지 않다
-        if (!content.content) {
+         if (!content.content) {
             processedCount++;
             if (processedCount === totalCount) {
                 alert("저장 성공!");
-                location.reload();
+                 location.reload();
             }
             return;
         }
@@ -180,4 +180,3 @@ function sendDataToServer(recordId, data) { // date_content랑 약간 다름
         });
     });
 }
-
